11567c0c378ffd613ffa5740d7c00ad4f4ba4130
Dorian Marchal <marchal.dorian@gmail.com>
2019-01-27 14:16:16 +0100

    Add a minimal repro for redux-saga#1743

--- index.js
+++ index.js
@@ -1,111 +1,55 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware, { END } from 'redux-saga';
-import { all, takeLatest, put, call, select } from 'redux-saga/effects';
-import { delay } from 'redux-saga';
+import { put, takeEvery } from 'redux-saga/effects';
 import _ from 'lodash';
 
-console.log('---');
-
-function reducer(state = {}, action) {
-  console.log({ action });
+// A simple reducer that counts PING and PONG actions.
+const initialState = { pingCount: 0, pongCount: 0 };
+function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'LOAD_PAGE':
-      state = { ...state, loadingPage: action.payload.name };
-      break;
-    case 'PAGE_LOADED':
-      state = { ...state, loadingPage: null, loadedPage: state.loadingPage };
-      break;
-    case 'HEADER':
-      state = { ...state, header: action.payload };
-      break;
-    case 'BODY':
-      state = { ...state, body: action.payload };
-      break;
-    case 'FOOTER':
-      state = { ...state, footer: action.payload };
-      break;
+    case 'PING':
+      return { ...state, pingCount: state.pingCount + 1 };
+    case 'PONG':
+      return { ...state, pongCount: state.pongCount + 1 };
   }
-
-  console.log({ state });
-  console.log('');
   return state;
 }
 
-const aLittleTime = () => (Math.random() + 0.3) * 1000;
-
-const sagaMiddleware = createSagaMiddleware();
-
-const bindMiddleware = (middlewares = []) => applyMiddleware(...middlewares);
-
-function* onLoadPage({ payload: { name } }) {
-  yield call(delay, aLittleTime());
-  // Update actions should be dispatched synchronously at the end of the page load saga.
-  // This avoids partial state change before the page change.
-  yield put({ type: 'HEADER', payload: name });
-  yield put({ type: 'BODY', payload: name });
-  yield put({ type: 'FOOTER', payload: name });
-  yield put({ type: 'PAGE_LOADED', payload: name });
+// Dispatch a PONG action.
+function* pong() {
+  yield put({ type: 'PONG' });
 }
 
+// On every PING action, the `pong` saga is started.
 function* rootSaga() {
-  yield all([takeLatest('LOAD_PAGE', onLoadPage)]);
+  yield takeEvery('PING', pong);
 }
 
-function configureStore(state = initialState) {
-  const store = createStore(reducer, state, bindMiddleware([sagaMiddleware]));
-  store.runSagaTask = () => {
-    if (_.isNil(store.sagaTask)) {
-      console.log('Run, saga!');
-      store.sagaTask = sagaMiddleware.run(rootSaga);
-    }
-  };
+// Create the store and the saga middleware.
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, undefined, applyMiddleware(sagaMiddleware));
 
-  store.stopSagaTask = () => {
-    if (!_.isNil(store.sagaTask)) {
-      console.log('Saga, stop!');
-      store.dispatch(END);
-      const { done } = store.sagaTask;
-      store.sagaTask = null;
-      return done;
-    }
-  };
+// Start saga.
+sagaMiddleware.run(rootSaga);
 
-  store.runSagaTask();
-  return store;
+// Dispatch a PING. We expect a PONG from saga.
+store.dispatch({ type: 'PING' });
+
+// End and restart saga.
+store.dispatch(END);
+sagaMiddleware.run(rootSaga);
+
+// Dispatch a PING. We expect an other PONG from the newly started saga.
+store.dispatch({ type: 'PING' });
+
+const expectedState = { pingCount: 2, pongCount: 2 };
+const currentState = store.getState();
+
+if (!_.isEqual(currentState, expectedState)) {
+  console.log(`ERROR:
+    Expected state: ${JSON.stringify(expectedState)},
+    Current state:  ${JSON.stringify(currentState)}
+  `);
+} else {
+  console.log('OK');
 }
-
-const store = configureStore({});
-
-const getInitialProps = async (name) => {
-  store.dispatch({ type: 'LOAD_PAGE', payload: { name } });
-  store.runSagaTask();
-  await new Promise((resolve) => {
-    const unsubscribe = store.subscribe(() => {
-      const { loadingPage } = store.getState();
-      if (loadingPage !== name) {
-        unsubscribe();
-        resolve();
-      }
-    });
-  });
-  console.log(`${name} getInitialProps ended.`);
-};
-
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-async function main() {
-  await wait(aLittleTime());
-  getInitialProps('home');
-  await wait(aLittleTime());
-  getInitialProps('news');
-  await wait(aLittleTime());
-  getInitialProps('contact');
-  await wait(aLittleTime());
-  getInitialProps('reviews');
-  await wait(aLittleTime());
-  getInitialProps('games');
-  await wait(aLittleTime());
-  getInitialProps('games');
-}
-
-main();
